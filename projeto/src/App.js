8df96c7a9a5512a1c5124e6df3b3d4a435be6d1e
Bruna Vieira Teixeira <bruna.v.teixeira@accenture.com>
2019-06-24 16:44:01 -0300

    adicionanto tweets

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,13 +1,73 @@
-import React from 'react';
-import Cabecalho from './components/Cabecalho/Cabecalho';
-import NavMenu from './components/NavMenu/NavMenu';
+import React, { Component, Fragment } from 'react';
+import Cabecalho from './components/Cabecalho'
+import NavMenu from './components/NavMenu'
+import Dashboard from './components/Dashboard'
+import Widget from './components/Widget'
+import TrendsArea from './components/TrendsArea'
+import Tweet from './components/Tweet'
 
-function App() {
-  return (
-    <Cabecalho>
-      <NavMenu nome="brunavieirat"></NavMenu>
-    </Cabecalho>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            novoTweet: '',
+            tweets: []
+        }
+    }
+
+    adicionaTweet = (event) => {
+        event.preventDefault();
+        // const novoTweet = this.state.novoTweet
+        // const anteriores = this.state.tweets
+        this.setState(stateAnterior => ({
+            tweets: [stateAnterior.novoTweet, ...stateAnterior.tweets],
+            novoTweet: ''
+        }))
+
+    }
+    render() {
+        return (
+            <Fragment>
+                <Cabecalho>
+                    <NavMenu usuario="@brunavieirat" />
+                </Cabecalho>
+                <div className="container">
+                    <Dashboard>
+                        <Widget>
+                            <form className="novoTweet" onSubmit={this.adicionaTweet}>
+                                <div className="novoTweet__editorArea">
+                                    <span className={`${this.state.novoTweet.length > 140 ? 'novoTweet__status--invalido' : 'novoTweet__status'}
+                                    `}> {this.state.novoTweet.length}/140</span>
+                                    <textarea
+                                        className="novoTweet__editor"
+                                        value={this.state.novoTweet}
+                                        placeholder="O que está acontecendo?"
+                                        onChange={(event) => this.setState({ novoTweet: event.target.value })}
+                                    ></textarea>
+                                </div>
+                                <button type="submit" className="novoTweet__envia"
+                                    disabled={this.state.novoTweet.length > 140 ? true : false}
+                                >Tweetar</button>
+                            </form>
+                        </Widget>
+                        <Widget>
+                            <TrendsArea />
+                        </Widget>
+                    </Dashboard>
+                    <Dashboard posicao="centro">
+                        <Widget>
+                            <div className="tweetsArea">
+                                {this.state.tweets.map((elemento, index) => (
+                                    <Tweet texto={elemento} key={index} />
+                                ))}
+
+                            </div>
+                        </Widget>
+                    </Dashboard>
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
--- projeto/src/index.js
+++ projeto/src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// CSSs Globais
+import "./assets/css/reset.css";
+import "./assets/css/container.css";
+import "./assets/css/btn.css";
+import "./assets/css/icon.css";
+import "./assets/css/iconHeart.css";
+import "./assets/css/notificacao.css";
+
+import "./assets/css/novoTweet.css";
+// import './index.css';
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
